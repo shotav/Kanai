c3e81ba4e3d5ef4c6ed58312e36a4abb087bdbd2
PryosCode <info@pryoscode.net>
2021-09-11 17:53:22 +0200

    Add cmrc

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <cmrc/cmrc.hpp>
+
+CMRC_DECLARE(foo);
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 const GLuint WIDTH = 800, HEIGHT = 600;
+FT_Library library;
 
 int main(int argc,char *argv[]) {
     glfwInit();
@@ -21,16 +27,32 @@ int main(int argc,char *argv[]) {
     glfwSetKeyCallback(window, key_callback);
     glfwSwapInterval(1);
 
+    auto fs = cmrc::foo::get_filesystem();
+    auto file = fs.open("res/test.txt");
+    std::cout << std::string(file.begin(), file.end()) << std::endl;
+
+    GLFWimage icons[1];
+    // auto icon = fs.open("res/icon.png");
+    // icons[0].pixels = ;
+    glfwSetWindowIcon(window, 1, icons);
+
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        glfwTerminate();
         return -1;
     }
 
     glViewport(0, 0, WIDTH, HEIGHT);
 
+    int error = FT_Init_FreeType(&library);
+    if (error) {
+        glfwTerminate();
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glfwSwapBuffers(window);
