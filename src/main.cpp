bc2179e4a6e3ed2826f3b9a0200a3539f237deed
PryosCode <info@pryoscode.net>
2021-09-11 23:38:32 +0200

    Add cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+#include <string>
 #include <iostream>
+#include <CLI/CLI.hpp>
 #include "resource.h"
 #include "window.h"
 
-int main(int argc,char *argv[]) {
+const std::string VERSION = "0.1.0";
+
+int main(int argc, char* argv[]) {
+    CLI::App app{"A lightweight cross-platform OpenGL text editor."};
+    bool version{false};
+    app.add_flag("-v,--version", version, "Print version");
+    std::string path;
+    app.add_option("PATH", path, "Path to open")->check(CLI::ExistingPath);
+    CLI11_PARSE(app, argc, argv);
+    if (version) {
+        std::cout << "Kanai v" << VERSION << std::endl;
+        return 0;
+    }
+
     std::cout << resource::load("res/test.txt") << std::endl;
 
     GLFWwindow* window = window::setup();
