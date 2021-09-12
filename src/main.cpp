9abfd5be311af6ab5d29620e2607e0b78f85e554
PryosCode <info@pryoscode.net>
2021-09-12 19:09:49 +0200

    Update Console Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,11 @@
 #include <string>
 #include <iostream>
-#include <CLI/CLI.hpp>
 #include "window.h"
-
-#ifdef _WIN32
-    #include <Windows.h>
-#endif
 
 const std::string VERSION = "0.1.0";
 
 int main(int argc, char* argv[]) {
-    CLI::App app{"A lightweight cross-platform OpenGL text editor."};
-    bool version{false};
-    app.add_flag("-v,--version", version, "Print version");
-    std::string path;
-    app.add_option("PATH", path, "Path to open")->check(CLI::ExistingPath);
-    CLI11_PARSE(app, argc, argv);
-    if (version) {
-        std::cout << "Kanai " << VERSION << std::endl;
-        return 0;
-    }
-
-    #ifdef _WIN32
-        FreeConsole();
-    #endif
+    std::string path = (argc > 1) ? argv[1] : "";
 
     GLFWwindow* window = window::setup();
     while (!glfwWindowShouldClose(window)) {
@@ -38,3 +20,11 @@ int main(int argc, char* argv[]) {
     glfwTerminate();
     return 0;
 }
+
+#ifdef _WIN32
+    #include <Windows.h>
+
+    int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+        return main(__argc, __argv);
+    }
+#endif
