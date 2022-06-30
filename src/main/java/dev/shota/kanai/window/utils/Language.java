c90f6e3a67889336ee5187107a93b03f34c2388a
PryosCode <info@pryoscode.net>
2022-06-30 23:49:08 +0200

    Update langs

--- src/main/java/dev/shota/kanai/window/utils/Language.java
+++ src/main/java/dev/shota/kanai/window/utils/Language.java
@@ -7,7 +7,7 @@ import java.util.ResourceBundle;
 @UtilityClass
 public class Language {
 
-    private static final ResourceBundle bundle = ResourceBundle.getBundle("language", Locale.getDefault());
+    private static final ResourceBundle bundle = ResourceBundle.getBundle("langs/language", Locale.getDefault());
 
     public static String get(String key) {
         return bundle.getString(key);
