a40cca5cabb97572d021213007262bbdcd4b00a0
shota <contact@shota.dev>
2022-07-01 14:35:22 +0200

    Update Menu

--- src/main/java/dev/shota/kanai/window/menu/Menu.java
+++ src/main/java/dev/shota/kanai/window/menu/Menu.java
@@ -3,7 +3,6 @@ package dev.shota.kanai.window.menu;
 import dev.shota.kanai.window.menu.edit.Edit;
 import dev.shota.kanai.window.menu.file.File;
 import dev.shota.kanai.window.menu.help.Help;
-
 import javax.swing.*;
 
 public class Menu extends JMenuBar {
